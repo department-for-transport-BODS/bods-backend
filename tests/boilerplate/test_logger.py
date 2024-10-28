a4481868a382267ca18124be4616ec90216298fa
andyecrow <38900570+andyecrow@users.noreply.github.com>
2024-10-28 17:21:35 +0100

    [BODS-7029] Add logger adapter to common layer (#11)

---
+++ tests/boilerplate/test_logger.py
@@ -0,0 +1,22 @@
+import pytest
+from src.boilerplate.logger import (
+    get_dataset_adapter_from_revision,
+    DatasetPipelineLoggerContext,
+)
+from unittest.mock import MagicMock
+
+
+def test_get_dataset_adapter_from_revision(caplog):
+    revision = MagicMock(id=1, dataset_id=2)
+    expected_logger_context = DatasetPipelineLoggerContext(
+        component_name="TimetablePipeline",
+        class_name="Dataset",
+        revision_id=1,
+        object_id=2,
+    )
+    logger = get_dataset_adapter_from_revision(revision)
+
+    assert logger.extra == {"context": expected_logger_context}
+    logger.info("Test Logger Call")
+    assert len(caplog.records) == 1
+    assert caplog.records[0].message == "[TimetablePipeline] Dataset 2 (Revision 1) => Test Logger Call"
