073b9f9e23f4293bcd5fe198e19765be62cdca19
Andy Crow <crow.andy96@gmail.com>
2024-10-29 16:24:56 +0100

    Formatting

--- src/boilerplate/exception.py
+++ src/boilerplate/exception.py
@@ -3,14 +3,15 @@ from typing import Optional
 from sqlalchemy.orm.exc import NoResultFound
 
 from common import BodsDB
+
 db = BodsDB()
 
 logger = logging.getLogger(__name__)
 
 
-        
 class PipelineException(Exception):
     """Basic exception for errors raised by a pipeline"""
+
     def __init__(self, message: Optional[str] = None):
         if message is None:
             # Set some default error message
