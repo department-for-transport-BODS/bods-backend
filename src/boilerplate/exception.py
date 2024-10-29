737107deec965d2838789ec85eb8beaefe611a50
Andy Crow <crow.andy96@gmail.com>
2024-10-29 16:24:56 +0100

    Formatting

--- src/boilerplate/exception.py
+++ src/boilerplate/exception.py
@@ -5,6 +5,7 @@ logger = logging.getLogger(__name__)
 
 class PipelineException(Exception):
     """Basic exception for errors raised by a pipeline"""
+
     def __init__(self, message: Optional[str] = None):
         if message is None:
             # Set some default error message
