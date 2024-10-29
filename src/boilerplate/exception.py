854db1ac84b226f1362202c2341c3b7977c7aa00
Andy Crow <crow.andy96@gmail.com>
2024-10-29 16:13:16 +0100

    Add DatasetETLTaskResultRepository + test

--- src/boilerplate/exception.py
+++ src/boilerplate/exception.py
@@ -3,10 +3,8 @@ from typing import Optional
 
 logger = logging.getLogger(__name__)
 
-
 class PipelineException(Exception):
     """Basic exception for errors raised by a pipeline"""
-
     def __init__(self, message: Optional[str] = None):
         if message is None:
             # Set some default error message
