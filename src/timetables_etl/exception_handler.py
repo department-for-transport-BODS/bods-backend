92e77fe0d47838208275ccb87295a1e0761f738a
Andy Crow <crow.andy96@gmail.com>
2024-11-12 12:22:41 +0100

    Add exception handler lambda + tests

--- src/timetables_etl/exception_handler.py
+++ src/timetables_etl/exception_handler.py
@@ -1,12 +1,11 @@
-from os import environ
 from pydantic import BaseModel
 from common import LambdaEvent
 from datetime import datetime
 from db.dataset_etl_task_result import DatasetETLTaskResultRepository
 from db.dataset_revision import DatasetRevisionRepository
-from enums import DatasetETLResultStatus
+from enums import DatasetETLResultStatus, FeedStatus
 from logger import get_dataset_adapter_from_revision, logger
-import json
+from utils.example_utils import my_util
 
 
 class ErrorInfo(BaseModel):
@@ -23,23 +22,27 @@ def lambda_handler(event, context):
     parsed_event = ExceptionEvent(**event)
     lambda_event = LambdaEvent(event)
 
-    taskResultRepo = DatasetETLTaskResultRepository(lambda_event.db)
-    datasetETLResult = taskResultRepo.get_by_id(parsed_event.DatasetEtlTaskResultId)
+    task_result_repo = DatasetETLTaskResultRepository(lambda_event.db)
+    dataset_etl_task_result = task_result_repo.get_by_id(parsed_event.DatasetEtlTaskResultId)
 
-    revisionRepo = DatasetRevisionRepository(lambda_event.db)
-    revision = revisionRepo.get_by_id(datasetETLResult.revision_id)
+    dataset_revision_repo = DatasetRevisionRepository(lambda_event.db)
+    revision = dataset_revision_repo.get_by_id(dataset_etl_task_result.revision_id)
+
     adapter = get_dataset_adapter_from_revision(revision.id, revision.dataset_id)
-    adapter.error(parsed_event.ErrorInfo.error_message, exc_info=True)
+    error_message = parsed_event.ErrorInfo.error_message
+    adapter.error(error_message, exc_info=True)
 
-    if datasetETLResult.status != DatasetETLResultStatus.FAILURE:
-        datasetETLResult.status = DatasetETLResultStatus.FAILURE
-        datasetETLResult.completed = datetime.now()
-        datasetETLResult.task_name_failed = parsed_event.ErrorInfo.step_name
-        datasetETLResult.error_code = DatasetETLResultStatus.SYSTEM_ERROR
-        datasetETLResult.additional_info = parsed_event.ErrorInfo.error_message
+    if dataset_etl_task_result.status != DatasetETLResultStatus.FAILURE:
+        dataset_etl_task_result.status = DatasetETLResultStatus.FAILURE
+        dataset_etl_task_result.completed = datetime.now()
+        dataset_etl_task_result.task_name_failed = parsed_event.ErrorInfo.step_name
+        dataset_etl_task_result.error_code = DatasetETLResultStatus.SYSTEM_ERROR
+        dataset_etl_task_result.additional_info = error_message
+        task_result_repo.update(dataset_etl_task_result)
 
-        taskResultRepo.update(datasetETLResult)
+        revision.status = FeedStatus.error
+        dataset_revision_repo.update(revision)
 
-    # TODO: Set revivision to error
-
-    return
+    return {
+        "statusCode": 200
+    }
