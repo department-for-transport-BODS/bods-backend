87e745aa642362f52def539fde7f7abef3ecc53e
Andy Crow <crow.andy96@gmail.com>
2024-11-19 11:54:31 +0100

    Add get_xml_file_pti_validator

--- src/timetables_etl/pti/validators/factory.py
+++ src/timetables_etl/pti/validators/factory.py
@@ -5,7 +5,6 @@ from timetables_etl.pti.validators.xml_file import XmlFilePTIValidator
 # TODO: Add this static file
 PTI_SCHEMA_PATH = Path(__file__) / "pti_schema.json"
 
-
 def get_xml_file_pti_validator() -> XmlFilePTIValidator:
     """
     Gets a PTI JSON Schema and returns a DatasetPTIValidator.
