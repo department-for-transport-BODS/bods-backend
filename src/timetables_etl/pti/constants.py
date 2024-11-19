84bec2c7e81265f5f76d31e870cf835acf57766d
Andy Crow <crow.andy96@gmail.com>
2024-11-19 11:54:53 +0100

    Add constants

---
+++ src/timetables_etl/pti/constants.py
@@ -0,0 +1,6 @@
+
+
+REF_URL = "http://pti_pdf_url.com/" # TODO: was coming from settings.PTI_PDF_URL. Hardcode or get from env var?
+REF_PREFIX = "Please refer to section "
+REF_SUFFIX = " in the BODS PTI profile v1.1 document: "
+NO_REF = "Please refer to the BODS PTI profile v1.1 document: "
