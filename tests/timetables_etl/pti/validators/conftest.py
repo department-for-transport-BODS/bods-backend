67ac7b1ca1996d153c97d3fbd50e692c684688a9
Andy Crow <crow.andy96@gmail.com>
2024-11-21 16:28:51 +0100

    Add service specific tests

--- tests/timetables_etl/pti/validators/conftest.py
+++ tests/timetables_etl/pti/validators/conftest.py
@@ -1,4 +1,21 @@
-from io import StringIO
+from io import StringIO, BytesIO
+
+from tests.timetables_etl.pti.validators.constants import TXC_END, TXC_START
+
+
+class XMLFile(BytesIO):
+    def __init__(self, str_, **kwargs):
+        super().__init__(**kwargs)
+        self.write(str_.encode("utf-8"))
+        self.name = "file.xml"
+        self.seek(0)
+
+
+class TXCFile(XMLFile):
+    def __init__(self, str_, **kwargs):
+        s = TXC_START + str_ + TXC_END
+        super().__init__(s, **kwargs)
+        self.name = "txc.xml"
 
 
 class JSONFile(StringIO):
