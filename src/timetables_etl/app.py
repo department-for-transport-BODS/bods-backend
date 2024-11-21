38d35d6e3676c8c0334b2471bbb46b0b481dec27
Rameshkanth Subramani <raks@Raks-Mac.local>
2024-11-21 14:45:53 +0000

    template update

---
+++ src/timetables_etl/app.py
@@ -0,0 +1,7 @@
+
+from logger import logger
+
+
+def lambda_handler(event, context):
+    logger.info("Template Lambda Function")
+    return
---
+++ tests/timetables_etl/test_app.py
@@ -0,0 +1,7 @@
+
+from timetables_etl.app import lambda_handler
+
+
+def test_app():
+    val = lambda_handler({}, None)
+    assert val is None
