a6b6140d0dd78cf54495d2db34a0dc301f28aaa7
Andy Crow <crow.andy96@gmail.com>
2024-11-22 15:36:39 +0100

    Fix test failure

--- tests/timetables_etl/pti/validators/test_stop_points.py
+++ tests/timetables_etl/pti/validators/test_stop_points.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from lxml import etree
 from pti.constants import PTI_PATH
 from pti.models import Schema
-from pti.validators.functions import validate_non_naptan_stop_points
 from pti.validators.pti import PTIValidator
 from tests.timetables_etl.pti.validators.conftest import JSONFile
 from tests.timetables_etl.pti.validators.factories import SchemaFactory
@@ -36,24 +35,3 @@ def test_validate(filename, expected):
     with txc_path.open("r") as txc:
         is_valid = pti.is_valid(txc)
     assert is_valid == expected
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    ("filename", "expected"),
-    [
-        ("stop_point_missing_mode.xml", False),
-        ("stop_point_w_bus_mode.xml", False),
-        ("stop_point_w_coach_mode.xml", True),
-        ("stop_point_w_bus_mode_success.xml", True),
-        ("stop_point_w_bus_mode_blank_enddate.xml", True),
-        ("stop_point_w_bus_mode_wo_operating_profile.xml", True),
-    ],
-)
-def test_check_stop_point_two_months(filename, expected):
-    NAMESPACE = {"x": "http://www.transxchange.org.uk/"}
-    string_xml = DATA_DIR / filename
-    with string_xml.open("r") as txc_xml:
-        doc = etree.parse(txc_xml)
-        elements = doc.xpath("//x:StopPoint", namespaces=NAMESPACE)
-        actual = validate_non_naptan_stop_points("", elements)
-        assert actual == expected
