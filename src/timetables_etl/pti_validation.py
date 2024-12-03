a2b96f5e8617b2aa93d71123a65ed4f85d1bfa5d
Andy Crow <crow.andy96@gmail.com>
2024-12-03 16:24:42 +0100

    Add lambda handler for pti validation

---
+++ src/timetables_etl/pti_validation.py
@@ -0,0 +1,49 @@
+
+
+
+
+from pydantic import BaseModel
+
+from common import DbManager
+from db.repositories.dataset_revision import DatasetRevisionRepository
+from db.repositories.txc_file_attributes import TxcFileAttributesRepository
+from exceptions.pipeline_exceptions import PipelineException
+from logger import get_dataset_adapter_from_revision
+from pti.service import PTIValidationService
+from s3 import S3
+
+
+class PTIValidationEvent(BaseModel):
+    DatasetRevisionId: int
+    Bucket: str
+    ObjectKey: str
+
+    
+def lambda_handler(event, context):
+    parsed_event = PTIValidationEvent(**event)
+
+    db = DbManager.get_db()
+
+    dataset_revision_repo = DatasetRevisionRepository(db)
+    revision = dataset_revision_repo.get_by_id(parsed_event.DatasetRevisionId)
+
+    adapter = get_dataset_adapter_from_revision(revision.id, revision.dataset_id)
+
+    s3_handler = S3(bucket_name=parsed_event.Bucket)
+    filename = parsed_event.ObjectKey
+    xml_file_object = s3_handler.get_object(file_path=filename)
+
+    txc_file_attributes_repo = TxcFileAttributesRepository(db)
+    txc_file_attributes = txc_file_attributes_repo.get(revision_id=revision.id, filename=filename)
+    if not txc_file_attributes:
+        message = f"Validation task: pti_validation, no valid file to process, zip file: {revision.upload_file}"
+        adapter.error(message, exc_info=True)
+        # TODO: Usually we mark the task as error here. Need to handle single file failure (use decorator?)
+        raise PipelineException(message)
+
+    validation_service = PTIValidationService(db)
+    validation_service.validate(revision, xml_file_object, txc_file_attributes)
+
+    return {
+        "statusCode": 200
+    }
---
+++ tests/timetables_etl/test_pti_validation.py
@@ -0,0 +1,57 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from db.repositories import txc_file_attributes
+from exceptions.pipeline_exceptions import PipelineException
+from pti_validation import lambda_handler
+
+
+@pytest.fixture(autouse=True, scope="module")
+def m_db_manager():
+    with patch("pti_validation.DbManager") as m_db:
+        yield m_db
+
+
+@patch("pti_validation.DatasetRevisionRepository")
+@patch("pti_validation.S3")
+@patch("pti_validation.TxcFileAttributesRepository")
+@patch("pti_validation.PTIValidationService")
+def test_lambda_hander(m_pti_validation_service, m_file_attribute_repo, m_s3, m_revision_repo):
+    revision_id = 123
+    event = {
+        "Bucket": "test-bucket",
+        "ObjectKey": "test-key",
+        "DatasetRevisionId": revision_id,
+    }
+
+    revision = MagicMock(id=revision_id, dataset_id=2)
+    m_revision_repo.return_value.get_by_id.return_value = revision
+
+    s3_file_obj = MagicMock()
+    m_s3.return_value.get_object.return_value = s3_file_obj
+
+    txc_file_attributes = MagicMock()
+    m_file_attribute_repo.return_value.get.return_value = txc_file_attributes
+
+    result = lambda_handler(event, {})
+
+    assert result == {"statusCode": 200}
+    m_s3.return_value.get_object.assert_called_once_with(file_path="test-key")
+    m_pti_validation_service.return_value.validate.assert_called_once_with(revision, s3_file_obj, txc_file_attributes)
+
+
+@patch("pti_validation.DatasetRevisionRepository")
+@patch("pti_validation.S3")
+@patch("pti_validation.TxcFileAttributesRepository")
+@patch("pti_validation.PTIValidationService")
+def test_lambda_hander_no_valid_files(m_pti_validation_service, m_file_attribute_repo, m_s3, m_revision_repo):
+    revision_id = 123
+    event = {
+        "Bucket": "test-bucket",
+        "ObjectKey": "test-key",
+        "DatasetRevisionId": revision_id,
+    }
+    m_file_attribute_repo.return_value.get.return_value = None
+
+    with pytest.raises(PipelineException):
+        lambda_handler(event, {})
