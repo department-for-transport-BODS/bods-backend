4851e5e3adc947bcfa7290d70914186862fc4378
Rob Steel <142216849+robsteel-kpmg@users.noreply.github.com>
2024-12-04 13:42:26 +0000

    [HOTFIX] Removed extra logs + optimised pt executions (#71)
    * Removed extra logs
    
    * Optimised iterator to maintain strict intervals;
    Updated SAM templates with stripped down SM functionality;
    Added option to switch b/w GTFS API endpoints
    
    ---------
    
    Co-authored-by: Amit Singh <amit.singh@kpmg.co.uk>

--- src/periodic_tasks/create_gtfsrt_zip.py
+++ src/periodic_tasks/create_gtfsrt_zip.py
@@ -6,9 +6,15 @@ from os import environ
 
 def lambda_handler(event, context):
     try:
+        CAVL_CONSUMER_URL = environ.get("CAVL_CONSUMER_URL", default="")
         GTFS_API_BASE_URL = environ.get("GTFS_API_BASE_URL", default="")
-        url = f"{GTFS_API_BASE_URL}/gtfs-rt"
-        _prefix = f"[GTFSRTArchiving] URL {url} => "
+        IS_NEW_GTFS_API_ACTIVE = environ.get("GTFS_API_ACTIVE", default=False) == "True"
+        url = (
+            f"{GTFS_API_BASE_URL}/gtfs-rt"
+            if IS_NEW_GTFS_API_ACTIVE
+            else f"{CAVL_CONSUMER_URL}/gtfsrtfeed"
+        )
+        _prefix = f"[GTFSRTArchiving] => "
         logger.info(_prefix + "Begin archiving GTFSRT data.")
         start = time.time()
         archiver = GTFSRTArchiver(event, url)
