f85f8e0c2c90d58c96a4edd2d3275a992260f219
Andy Crow <crow.andy96@gmail.com>
2024-12-19 19:22:42 +0000

    Update lambda handler tests

--- tests/timetables_etl/test_pti_validation.py
+++ tests/timetables_etl/test_pti_validation.py
@@ -1,10 +1,17 @@
+from io import BytesIO
 from unittest.mock import MagicMock, patch
 
 import pytest
+from botocore.response import StreamingBody
 from common_layer.db.constants import StepName
+from common_layer.dynamodb.models import TXCFileAttributes
 from common_layer.exceptions.pipeline_exceptions import PipelineException
 
 from tests.conftest import decorator_mock
+from tests.timetables_etl.factories.database.organisation import (
+    OrganisationDatasetRevisionFactory,
+    OrganisationTXCFileAttributesFactory,
+)
 
 # Patch the file processing decorator before importing the module
 with patch(
@@ -28,13 +35,19 @@ def m_db_manager():
         yield m_db
 
 
+@patch("pti_validation.FileProcessingDataManager")
 @patch("pti_validation.DynamoDB")
-@patch("pti_validation.DatasetRevisionRepository")
+@patch("pti_validation.OrganisationDatasetRevisionRepo")
 @patch("pti_validation.S3")
-@patch("pti_validation.TxcFileAttributesRepository")
+@patch("pti_validation.OrganisationTXCFileAttributesRepo")
 @patch("pti_validation.PTIValidationService")
 def test_lambda_hander(
-    m_pti_validation_service, m_file_attribute_repo, m_s3, m_revision_repo, m_dynamodb
+    m_pti_validation_service,
+    m_file_attribute_repo,
+    m_s3,
+    m_revision_repo,
+    m_dynamodb,
+    m_data_manager,
 ):
     revision_id = 123
     event = {
@@ -43,41 +56,55 @@ def test_lambda_hander(
         "DatasetRevisionId": revision_id,
     }
 
-    revision = MagicMock(id=revision_id, dataset_id=2)
+    revision = OrganisationDatasetRevisionFactory.create_with_id(id_number=revision_id)
     m_revision_repo.return_value.get_by_id.return_value = revision
 
     s3_file_obj = MagicMock()
+    s3_file_obj = StreamingBody(BytesIO(b"<xml-content>"), len(b"<xml-content>"))
     m_s3.return_value.get_object.return_value = s3_file_obj
 
-    txc_file_attributes = MagicMock()
-    m_file_attribute_repo.return_value.get.return_value = txc_file_attributes
+    txc_file_attributes = OrganisationTXCFileAttributesFactory.create()
+    m_file_attribute_repo.return_value.get_by_revision_id_and_filename.return_value = (
+        txc_file_attributes
+    )
+    expected_file_attributes = TXCFileAttributes.from_orm(txc_file_attributes)
 
     result = pti_validation.lambda_handler(event, {})
 
     assert result == {"statusCode": 200}
     m_s3.return_value.get_object.assert_called_once_with(file_path="test-key")
     m_pti_validation_service.return_value.validate.assert_called_once_with(
-        revision, s3_file_obj, txc_file_attributes
+        revision, s3_file_obj, expected_file_attributes
     )
     m_file_processing_result_to_db.assert_called_once_with(
         step_name=StepName.PTI_VALIDATION
     )
 
 
-@patch("pti_validation.DatasetRevisionRepository")
+@patch("pti_validation.FileProcessingDataManager")
+@patch("pti_validation.DynamoDB")
+@patch("pti_validation.OrganisationDatasetRevisionRepo")
 @patch("pti_validation.S3")
-@patch("pti_validation.TxcFileAttributesRepository")
+@patch("pti_validation.OrganisationTXCFileAttributesRepo")
 @patch("pti_validation.PTIValidationService")
 def test_lambda_hander_no_valid_files(
-    m_pti_validation_service, m_file_attribute_repo, m_s3, m_revision_repo
+    m_pti_validation_service,
+    m_file_attribute_repo,
+    m_s3,
+    m_revision_repo,
+    m_dynamodb,
+    m_data_manager,
 ):
+
     revision_id = 123
     event = {
         "Bucket": "test-bucket",
         "ObjectKey": "test-key",
         "DatasetRevisionId": revision_id,
     }
-    m_file_attribute_repo.return_value.get.return_value = None
+    m_file_attribute_repo.return_value.get_by_revision_id_and_filename.return_value = (
+        None
+    )
 
     with pytest.raises(PipelineException):
         pti_validation.lambda_handler(event, {})
