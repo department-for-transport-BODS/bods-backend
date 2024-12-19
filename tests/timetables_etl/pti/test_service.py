6cd1cbf613d786c35d04946741a98438b2d831d7
Andy Crow <crow.andy96@gmail.com>
2024-12-19 17:49:52 +0000

    Replace queries in PTIValidationService with cached data

--- tests/timetables_etl/pti/test_service.py
+++ tests/timetables_etl/pti/test_service.py
@@ -1,56 +1,52 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
-from pti.service import PTIValidationService
 from common_layer.pti.models import Violation
-
-
-@pytest.fixture(autouse=True, scope="module")
-def m_dataset_repository():
-    with patch("pti.service.DatasetRepository") as m_repo:
-        yield m_repo
+from pti.service import PTIValidationService
 
 
 @patch("pti.service.PTIObservationRepository")
 @patch("pti.service.TXCRevisionValidator")
 @patch("pti.service.get_xml_file_pti_validator")
-@patch("pti.service.TxcFileAttributesRepository")
 def test_validate(
-    m_file_attributes_repository, m_get_xml_file_pti_validator, m_txc_revision_validator, m_observation_repo
+    m_get_xml_file_pti_validator, m_txc_revision_validator, m_observation_repo
 ):
     revision = MagicMock(id=123)
     xml_file = MagicMock()
     xml_file.read.return_value = b"dummycontent"
     txc_file_attributes = MagicMock()
 
-    m_file_attributes_repository.return_value.exists.return_value = False
-
     violations = [
         Violation.model_construct(),
         Violation.model_construct(),
     ]
     m_get_xml_file_pti_validator.return_value.get_violations.return_value = violations
-    m_txc_revision_validator.return_value.get_violations.return_value = ["txc_revision_violation"]
+    m_txc_revision_validator.return_value.get_violations.return_value = [
+        "txc_revision_violation"
+    ]
 
-    service = PTIValidationService(db=MagicMock(), dynamodb=MagicMock())
+    service = PTIValidationService(
+        db=MagicMock(), dynamodb=MagicMock(), live_revision_attributes=[]
+    )
     service.validate(revision, xml_file, txc_file_attributes)
 
-    m_get_xml_file_pti_validator.return_value.get_violations.assert_called_once_with(revision, xml_file)
-    m_observation_repo.return_value.create.assert_called_once_with(revision.id, violations)
+    m_get_xml_file_pti_validator.return_value.get_violations.assert_called_once_with(
+        revision, xml_file
+    )
+    m_observation_repo.return_value.create.assert_called_once_with(
+        revision.id, violations
+    )
 
 
 @patch("pti.service.PTIObservationRepository")
 @patch("pti.service.sha1sum")
 @patch("pti.service.TXCRevisionValidator")
 @patch("pti.service.get_xml_file_pti_validator")
-@patch("pti.service.TxcFileAttributesRepository")
 def test_validate_unchanged_file(
-    m_file_attributes_repository,
     m_get_xml_file_pti_validator,
     m_txc_revision_validator,
     m_sha1_sum,
     m_observation_repo,
-    m_dataset_repository,
 ):
     """
     Validation should be skipped for unchanged files
@@ -59,19 +55,20 @@ def test_validate_unchanged_file(
     xml_file = MagicMock()
     xml_file.read.return_value = b"dummycontent"
 
+    existing_file_hash = "file-hash"
+    m_sha1_sum.return_value = existing_file_hash
+
     txc_file_attributes = MagicMock()
-    m_file_attributes_repository.return_value.exists.return_value = True
+    live_file_attributes = [MagicMock(hash=existing_file_hash)]
 
-    m_dataset = MagicMock()
-    m_dataset_repository.return_value.get_by_id.return_value = m_dataset
-
-    service = PTIValidationService(db=MagicMock(), dynamodb=MagicMock())
+    service = PTIValidationService(
+        db=MagicMock(),
+        dynamodb=MagicMock(),
+        live_revision_attributes=live_file_attributes,
+    )
     service.validate(revision, xml_file, txc_file_attributes)
 
-    # We should look for the file in the live revision of the dataset
-    m_file_attributes_repository.return_value.exists.assert_called_once_with(
-        revision_id=m_dataset.live_revision_id, hash=m_sha1_sum.return_value
-    )
+    m_sha1_sum.assert_called_once_with(xml_file.read.return_value)
 
     # Validations skipped
     m_get_xml_file_pti_validator.return_value.get_violations.assert_not_called()
