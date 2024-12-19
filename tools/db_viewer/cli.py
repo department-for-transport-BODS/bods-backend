1cd7ec19c5ec6e03a0f35743c6ebcf6a7daff7f2
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2024-12-19 17:41:58 +0000

    Fix DB Viewer for Services (#108)
    - Remove Dependency on revision_id when fetching by service_id
    - Fetch Naptan stops by atco codes, not locality ID
    - Use validate_params function
    - Add default output path creation so it's not in generated in the root
      - Base path is ./data/db_viewer/ with either revision_id/<id> or service_id/<id>
    - Fix Type errors and remove ignores on the types

--- tools/db_viewer/cli.py
+++ tools/db_viewer/cli.py
@@ -1,119 +1,171 @@
+"""
+Database View Tool to search by revision id or service ID
+"""
+
 from pathlib import Path
-from typer import Typer, Option, BadParameter
-from structlog.stdlib import get_logger
+
 from common_layer.database.models import NaptanStopPoint
+from common_layer.database.models.model_transmodel import (
+    TransmodelServicePattern,
+    TransmodelServicePatternStop,
+)
 from common_layer.database.repos import NaptanStopPointRepo
+from structlog.stdlib import get_logger
+from typer import BadParameter, Option, Typer
+
 from .config import DbConfig
 from .organisation import (
-    extract_dataset_revision,
     extract_dataset,
+    extract_dataset_revision,
     extract_organisation,
     extract_txc_attributes,
 )
 from .transmodel import (
-    extract_service_by_revision_id,
-    extract_service_by_id,
-    extract_servicepattern,
-    extract_servicepatternstop,
-    extract_stopactivity,
-    extract_service_service_patterns,
-    extract_servicepattern_localities,
-    extract_vehiclejourney,
     extract_bookingarrangements,
     extract_flexibleserviceoperationperiod,
-    extract_servicedorganisationvehiclejourney,
+    extract_service_by_id,
+    extract_service_service_patterns,
     extract_servicedorganisations,
+    extract_servicedorganisationvehiclejourney,
     extract_servicedorganisationworkingdays,
+    extract_servicepattern_localities,
+    extract_servicepatterns_by_ids,
+    extract_servicepatterns_by_revision_id,
+    extract_servicepatternstop,
+    extract_services_by_revision_id,
+    extract_stopactivity,
+    extract_vehiclejourney,
 )
-from .utils import (
-    get_db_instance,
-    SqlDB,
-    csv_extractor,
-)
+from .utils import SqlDB, csv_extractor, get_db_instance
 
 logger = get_logger()
 app = Typer()
 
 
 @csv_extractor()
-def extract_stoppoint(db: SqlDB, locality_ids: list[int]) -> list[NaptanStopPoint]:
+def extract_stoppoint(db: SqlDB, atco_codes: list[str]) -> list[NaptanStopPoint]:
     """
     Extract naptan stoppoint details from DB.
     """
     repo = NaptanStopPointRepo(db)
-    return repo.get_by_locality_ids(locality_ids)
+    stop_points, missing_stops = repo.get_by_atco_codes(atco_codes)
+    if missing_stops:
+        logger.warning("Some Stops were not found in DB", missing_stops=missing_stops)
+    return stop_points
 
 
-def process_data_extraction(
+def process_vehicle_journeys(
     db: SqlDB,
-    revision_id: int,
-    service_id: int | None = None,
-    output_path: Path | str | None = None,
+    service_pattern_stops: list[TransmodelServicePatternStop],
+    output_path: Path,
 ):
-    log = logger.bind(operation="batch_extraction")
-    log.info("Starting DB Data extraction and output to CSV")
-
-    if service_id:
-        result = extract_service_by_id(service_id)
-        revision_id = result.id
-
-    # Organisation related entity
-    result = extract_dataset_revision(db, revision_id, output_path=output_path)
-    dataset_id = result.dataset_id
-    result = extract_dataset(db, dataset_id, output_path=output_path)
-    organisation_id = result.organisation_id
-    extract_organisation(db, organisation_id, output_path=output_path)
-    extract_txc_attributes(db, revision_id, output_path=output_path)
-
-    # Transmodel related entities
-    results = extract_service_by_revision_id(db, revision_id, output_path=output_path)
-    service_ids = list(set([result.id for result in results]))
-
-    results = extract_servicepattern(db, revision_id, output_path=output_path)
-    service_pattern_ids = list(set([result.id for result in results]))
-
-    results = extract_servicepatternstop(
-        db, service_pattern_ids, output_path=output_path
-    )
-    stop_activity_ids = list(set([result.stop_activity_id for result in results]))
-    vehicle_journey_ids = list(set([result.vehicle_journey_id for result in results]))
+    """
+    Process Vehicle Journeys
+    """
+    vehicle_journey_ids = [
+        result.vehicle_journey_id for result in service_pattern_stops
+    ]
     extract_vehiclejourney(db, vehicle_journey_ids, output_path=output_path)
-
-    results = extract_servicepattern_localities(
-        db, service_pattern_ids, output_path=output_path
-    )
-
-    extract_stoppoint(
-        db,
-        list(set([result.locality_id for result in results])),
-        output_path=output_path,
-    )
-
-    extract_stopactivity(db, stop_activity_ids, output_path=output_path)
-
-    extract_service_service_patterns(db, service_ids, output_path=output_path)
-
-    extract_bookingarrangements(db, service_ids, output_path=output_path)
     extract_flexibleserviceoperationperiod(
         db, vehicle_journey_ids, output_path=output_path
     )
-    results = extract_servicedorganisationvehiclejourney(
+    serviced_org_vj = extract_servicedorganisationvehiclejourney(
         db, vehicle_journey_ids, output_path=output_path
     )
-
     extract_servicedorganisations(
         db,
-        list(set([it.serviced_organisation_id for it in results])),
+        [it.serviced_organisation_id for it in serviced_org_vj],
         output_path=output_path,
     )
     extract_servicedorganisationworkingdays(
         db,
-        list(set([it.id for it in results])),
+        [it.id for it in serviced_org_vj],
         output_path=output_path,
     )
 
 
-def validate_params(revision_id: int, service_id: int) -> None:
+def process_service_patterns(
+    db: SqlDB,
+    tm_service_patterns: list[TransmodelServicePattern],
+    output_path: Path,
+):
+    """
+    Items dependent on service patterns
+    """
+    service_pattern_ids = [result.id for result in tm_service_patterns]
+    service_pattern_stops = extract_servicepatternstop(
+        db, service_pattern_ids, output_path=output_path
+    )
+    extract_servicepattern_localities(db, service_pattern_ids, output_path=output_path)
+    extract_stoppoint(
+        db,
+        [result.atco_code for result in service_pattern_stops],
+        output_path=output_path,
+    )
+    stop_activity_ids = [result.stop_activity_id for result in service_pattern_stops]
+
+    extract_stopactivity(db, stop_activity_ids, output_path=output_path)
+    process_vehicle_journeys(db, service_pattern_stops, output_path)
+
+
+def process_from_service_id(
+    db: SqlDB,
+    service_id: int,
+    output_path: Path,
+):
+    """
+    Process data from Service ID
+    """
+    log = logger.bind(operation="batch_extraction")
+    service = extract_service_by_id(db, service_id, output_path=output_path)
+    if service is None:
+        log.error("Transmodel Service Not Found")
+        raise BadParameter("Transmodel Service was not found in Database")
+    extract_bookingarrangements(db, [service.id], output_path=output_path)
+    service_service_patterns = extract_service_service_patterns(
+        db, [service.id], output_path=output_path
+    )
+    service_pattern_ids = [sp.servicepattern_id for sp in service_service_patterns]
+    service_patterns = extract_servicepatterns_by_ids(db, service_pattern_ids)
+    process_service_patterns(db, service_patterns, output_path=output_path)
+
+
+def process_from_revision_id(
+    db: SqlDB,
+    revision_id: int,
+    output_path: Path,
+):
+    """
+    Extract Data from DB and Output to CSVs based on table name
+    """
+    log = logger.bind(operation="batch_extraction")
+    log.info("Starting DB Data extraction and output to CSV")
+
+    # Organisation related entity
+    dataset_revision = extract_dataset_revision(
+        db, revision_id, output_path=output_path
+    )
+    dataset = extract_dataset(db, dataset_revision.dataset_id, output_path=output_path)
+    if dataset:
+        extract_organisation(db, dataset.organisation_id, output_path=output_path)
+    extract_txc_attributes(db, revision_id, output_path=output_path)
+
+    # Transmodel related entities
+    tm_services = extract_services_by_revision_id(
+        db, revision_id, output_path=output_path
+    )
+    service_ids = [it.id for it in tm_services]
+
+    extract_service_service_patterns(db, service_ids, output_path=output_path)
+
+    extract_bookingarrangements(db, service_ids, output_path=output_path)
+    service_patterns = extract_servicepatterns_by_revision_id(
+        db, revision_id, output_path=output_path
+    )
+    process_service_patterns(db, service_patterns, output_path)
+
+
+def validate_params(revision_id: int | None, service_id: int | None) -> None:
     """
     Validate command line parameter
     """
@@ -127,11 +179,36 @@ def validate_params(revision_id: int, service_id: int) -> None:
         )
 
 
+def make_default_output_path(revision_id: int | None, service_id: int | None) -> Path:
+    """
+    Generate a default output path based on revision_id or service_id.
+    Creates directories if they don't exist.
+
+    Base path is ./data/db_viewer/ with either revision_id/<id> or service_id/<id>
+    """
+    base_path = Path("./data/db_viewer")
+
+    if not base_path.exists():
+        logger.info("Creating base directory structure", path=str(base_path))
+        base_path.mkdir(parents=True, exist_ok=True)
+
+    if revision_id is not None:
+        final_path = base_path / "revision_id" / str(revision_id)
+    else:
+        final_path = base_path / "service_id" / str(service_id)
+
+    if not final_path.exists():
+        logger.info("Creating output directory", path=str(final_path))
+        final_path.mkdir(parents=True, exist_ok=True)
+
+    return final_path
+
+
 @app.command()
 def main(
-    location: Path = Option(
+    output_path: Path | None = Option(
         None,
-        "--dir",
+        "--output-path",
         help="Paths to csv files",
     ),
     db_host: str = Option(
@@ -159,12 +236,12 @@ def main(
         "--db-port",
         help="Database port",
     ),
-    revision_id: int = Option(
+    revision_id: int | None = Option(
         None,
         "--revision-id",
         help="Dataset revision id",
     ),
-    service_id: int = Option(
+    service_id: int | None = Option(
         None,
         "--service-id",
         help="Service id",
@@ -174,13 +251,20 @@ def main(
     This tool queries a database then creates CSVs for ETL data for a
     specific revision id or service id
     """
+    validate_params(revision_id, service_id)
     config = DbConfig(
         host=db_host, port=db_port, user=db_user, password=db_password, database=db_name
     )
     db = get_db_instance(config)
-    process_data_extraction(
-        db=db, revision_id=revision_id, service_id=service_id, output_path=location
-    )
+    if output_path is None:
+        output_path = make_default_output_path(revision_id, service_id)
+    if revision_id:
+        process_from_revision_id(
+            db=db, revision_id=revision_id, output_path=output_path
+        )
+    if service_id:
+        process_from_service_id(db, service_id, output_path)
+    logger.info("Completed Processing", output_path=output_path)
 
 
 if __name__ == "__main__":
