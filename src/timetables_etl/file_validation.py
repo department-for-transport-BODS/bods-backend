338c730420364aa4de486bc5aa9dc2d29c8887a5
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-01-08 12:28:51 +0000

    Tidy and add some logs

--- src/timetables_etl/file_validation.py
+++ src/timetables_etl/file_validation.py
@@ -1,4 +1,9 @@
+"""
+FileValidation Lambda
+"""
+
 from io import BytesIO
+from xml.etree.ElementTree import ElementTree
 
 from common_layer.db.constants import StepName
 from common_layer.db.file_processing_result import file_processing_result_to_db
@@ -26,14 +31,19 @@ class FileValidationInputData(BaseModel):
     s3_file_key: str = Field(alias="ObjectKey")
 
 
-def dangerous_xml_check(file_object: BytesIO):
+def dangerous_xml_check(file_object: BytesIO) -> ElementTree:
     """
     Parse and check the file object syntax error
     """
     try:
-        detree.parse(
+        parsed_xml = detree.parse(
             file_object, forbid_dtd=True, forbid_entities=True, forbid_external=True
         )
+        log.info(
+            "XML successfully validated with no dangerous content",
+            file_name=file_object.name,
+        )
+        return parsed_xml
     except detree.ParseError as err:
         log.error("XML syntax error", exc_info=True)
         raise XMLSyntaxError(file_object.name, message=err.msg) from err
@@ -47,12 +57,18 @@ def get_xml_file_object(s3_bucket: str, s3_key: str) -> BytesIO:
     Download XML from S3
     """
     s3_client = S3(s3_bucket)
-    return s3_client.download_fileobj(s3_key)
+    file_obj = s3_client.download_fileobj(s3_key)
+    file_size = file_obj.getbuffer().nbytes
+    file_obj.seek(0)
+    log.info(
+        "Downloaded XML file", file_size_bytes=file_size, bucket=s3_bucket, key=s3_key
+    )
+    return file_obj
 
 
 def is_xml_file(file_name: str) -> bool:
     """
-    Check file name whether its a xml file
+    Check file extension ends in .xml
     """
     if not file_name.lower().endswith(".xml"):
         log.error("File is not a xml file", file_name=file_name)
@@ -60,25 +76,24 @@ def is_xml_file(file_name: str) -> bool:
     return True
 
 
-def process_file_validation(input_data: FileValidationInputData):
+def process_file_validation(input_data: FileValidationInputData) -> None:
     """
     Process the file validation
     """
     log.info("Processing file validation", file_name=input_data.s3_file_key)
-    try:
-        is_xml_file(input_data.s3_file_key)
-        dangerous_xml_check(
-            get_xml_file_object(input_data.s3_bucket_name, input_data.s3_file_key)
-        )
-    except Exception as excep:
-        raise excep
+    is_xml_file(input_data.s3_file_key)
+    xml_file_data = get_xml_file_object(
+        input_data.s3_bucket_name, input_data.s3_file_key
+    )
+    dangerous_xml_check(xml_file_data)
     log.info("File validation passed")
 
 
 @file_processing_result_to_db(step_name=StepName.TXC_FILE_VALIDATOR)
-def lambda_handler(event, _):
+def lambda_handler(event, _context) -> dict[str, str | int]:
     """
     Lambda handler for file validation
     """
-    process_file_validation(FileValidationInputData(**event))
+    input_data = FileValidationInputData(**event)
+    process_file_validation(input_data)
     return {"statusCode": 200, "body": "Completed File Validation"}
