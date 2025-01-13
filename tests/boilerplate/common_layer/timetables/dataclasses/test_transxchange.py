78db69a751c93a8d0f9d8c576a50f0802f78e6a9
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-01-13 18:06:44 +0000

    Revert "Remove Timetables folder"
    This reverts commit 2bcebb23fb9c08522398c66be05aa28b53eb2714.

---
+++ tests/boilerplate/common_layer/timetables/dataclasses/test_transxchange.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import unittest
+from common_layer.timetables.transxchange import TransXChangeDocument
+from common_layer.timetables.dataclasses import transxchange
+
+
+class TestTransXChangeZip(unittest.TestCase):
+    def setUp(self):
+        xml_path = Path(__file__).parent.parent.parent.resolve() / "data" / "sample.xml"
+        xml_content = open(xml_path, "rb")
+        self.document = TransXChangeDocument(xml_content)
+
+    def test_from_txc_document(self):
+        buf = transxchange.TXCFile.from_txc_document(self.document,
+                                                     True)
+        self.assertEqual(buf.header.filename, "sample.xml")
+        self.assertEqual(buf.header.schema_version, "2.4")
+        self.assertEqual(buf.service.service_code, "UZ000FLIX:UKN603")
+        self.assertEqual(buf.operator.national_operator_code, "FLIX")
+        self.assertEqual(buf.service_code, "UZ000FLIX:UKN603")
+        self.assertEqual(buf.hash, "ca6a517e77b727d34a05de63938777f5199bb1f6")
+
+
+
