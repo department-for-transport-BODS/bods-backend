851a4fc71bc841f99f58fa8423b08396badc39c1
Rameshkanth Subramani <raks@Raks-Mac.local>
2025-01-16 10:21:01 +0000

    black formatting

--- src/boilerplate/common_layer/database/repos/repo_etl_task.py
+++ src/boilerplate/common_layer/database/repos/repo_etl_task.py
@@ -53,7 +53,7 @@ class ETLTaskResultRepo(BaseRepository[DatasetETLTaskResult]):
             task.status = TaskState.SUCCESS
             task.completed = datetime.now(UTC)
             task.task_name_failed = ""
-            task.error_code = None
+            task.error_code = ""
 
         statement = self._build_query().where(self._model.id == task_id)
         self._update_one(statement, update_func)
