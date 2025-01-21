d1f93120aed78903c1f03d54d03a74d9a1853c6f
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-01-21 16:43:05 +0000

    Move FileValidation Lambda into subfolder (#190)
    - Move File Validation lambda into subfolder
    - Split into multiple files

--- src/timetables_etl/file_validation/app/file_validation.py
+++ src/timetables_etl/file_validation/app/file_validation.py
@@ -3,54 +3,18 @@ FileValidation Lambda
 """
 
 from io import BytesIO
-from xml.etree.ElementTree import ElementTree
 
 from aws_lambda_powertools import Tracer
 from common_layer.db.constants import StepName
 from common_layer.db.file_processing_result import file_processing_result_to_db
-from common_layer.exceptions.xml_file_exceptions import (
-    DangerousXML,
-    FileNotXML,
-    XMLSyntaxError,
-)
 from common_layer.s3 import S3
-from defusedxml import DefusedXmlException
-from defusedxml import ElementTree as detree
-from pydantic import BaseModel, Field
 from structlog.stdlib import get_logger
+
+from .models import FileValidationInputData
+from .xml_checks import dangerous_xml_check, is_xml_file
 
 tracer = Tracer()
 log = get_logger()
-
-
-class FileValidationInputData(BaseModel):
-    """
-    Input data for the File Validation
-    """
-
-    revision_id: int = Field(alias="DatasetRevisionId")
-    s3_bucket_name: str = Field(alias="Bucket")
-    s3_file_key: str = Field(alias="ObjectKey")
-
-
-def dangerous_xml_check(file_object: BytesIO, file_name: str) -> ElementTree:
-    """
-    Parse and check the file object syntax error
-    """
-    try:
-        parsed_xml = detree.parse(
-            file_object, forbid_dtd=True, forbid_entities=True, forbid_external=True
-        )
-        log.info(
-            "XML successfully validated with no dangerous content",
-        )
-        return parsed_xml
-    except detree.ParseError as err:
-        log.error("XML syntax error", exc_info=True)
-        raise XMLSyntaxError(file_name, message=err.msg) from err
-    except DefusedXmlException as err:
-        log.error("Dangerous XML", exc_info=True)
-        raise DangerousXML(file_name, message=err) from err
 
 
 def get_xml_file_object(s3_bucket: str, s3_key: str) -> BytesIO:
@@ -65,16 +29,6 @@ def get_xml_file_object(s3_bucket: str, s3_key: str) -> BytesIO:
         "Downloaded XML file", file_size_bytes=file_size, bucket=s3_bucket, key=s3_key
     )
     return file_obj
-
-
-def is_xml_file(file_name: str) -> bool:
-    """
-    Check file extension ends in .xml
-    """
-    if not file_name.lower().endswith(".xml"):
-        log.error("File is not a xml file", file_name=file_name)
-        raise FileNotXML(file_name)
-    return True
 
 
 def process_file_validation(input_data: FileValidationInputData) -> None:
