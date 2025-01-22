440b60f50ae7d8eeef66e5b7649de770bd43f184
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-01-22 12:34:29 +0000

    FIx Lint errors in enums.py

--- src/boilerplate/common_layer/enums.py
+++ src/boilerplate/common_layer/enums.py
@@ -1,22 +1,34 @@
+"""
+Common Enums
+"""
+
 from enum import Enum, unique
 
 
 @unique
 class CAVLDataFormat(Enum):
+    """
+    CAVL Data Format Types
+    """
+
     SIRIVM = "VM"
     GTFSRT = "RT"
     SIRIVM_TFL = "TL"
 
 
 class FeedStatus(str, Enum):
-    pending = "pending"
-    draft = "draft"
-    indexing = "indexing"
-    live = "live"
-    success = "success"
-    expiring = "expiring"
-    warning = "warning"
-    error = "error"
-    expired = "expired"
-    deleted = "deleted"
-    inactive = "inactive"
+    """
+    Revision Status Field Enum
+    """
+
+    PENDING = "pending"
+    DRAFT = "draft"
+    INDEXING = "indexing"
+    LIVE = "live"
+    SUCCESS = "success"
+    EXPIRING = "expiring"
+    WARNING = "warning"
+    ERROR = "error"
+    EXPIRED = "expired"
+    DELETED = "deleted"
+    INACTIVE = "inactive"
