92765d8925fdd812a4403c995007e757e43e51a0
Andy Crow <crow.andy96@gmail.com>
2025-01-27 13:38:46 +0100

    More test fixes

--- tests/timetables_etl/pti/validators/test_factory.py
+++ tests/timetables_etl/pti/validators/test_factory.py
@@ -11,6 +11,7 @@ from common_layer.dynamodb.client.naptan_stop_points import (
     NaptanStopPointDynamoDBClient,
 )
 from common_layer.txc.models.txc_data import TXCData
+from pti.app.models.models_pti_task import DbClients
 from pti.app.validators.factory import get_xml_file_pti_validator
 from pti.app.validators.xml_file import XmlFilePTIValidator
 
@@ -28,6 +29,10 @@ def test_get_xml_file_pti_validator(
     m_dynamodb = MagicMock(spec=DynamoDBCache)
     m_stop_point_client = MagicMock(spec=NaptanStopPointDynamoDBClient)
     m_db = MagicMock(spec=SqlDB)
+    clients = DbClients(
+        sql_db=m_db, dynamodb=m_dynamodb, stop_point_client=m_stop_point_client
+    )
+
     txc_data = TXCData.model_construct()
     mock_schema_path.return_value = MagicMock(spec=Path)
     mock_open_fn = mock_open(read_data='{"key": "value"}')
@@ -37,15 +42,11 @@ def test_get_xml_file_pti_validator(
     mock_validator_class.return_value = mock_validator_instance
 
     result = get_xml_file_pti_validator(
-        dynamodb=m_dynamodb,
-        stop_point_client=m_stop_point_client,
-        db=m_db,
+        db_clients=clients,
         txc_data=txc_data,
     )
 
     # Assertions
     mock_open_fn.assert_called_once_with("r")
-    mock_validator_class.assert_called_once_with(
-        mock_open_fn(), m_dynamodb, m_stop_point_client, m_db, txc_data
-    )
+    mock_validator_class.assert_called_once_with(mock_open_fn(), clients, txc_data)
     assert result == mock_validator_instance
--- tests/timetables_etl/pti/validators/test_xml_file.py
+++ tests/timetables_etl/pti/validators/test_xml_file.py
@@ -10,6 +10,7 @@ from common_layer.database.client import SqlDB
 from common_layer.dynamodb.client import NaptanStopPointDynamoDBClient
 from common_layer.dynamodb.client.cache import DynamoDBCache
 from common_layer.txc.models.txc_data import TXCData
+from pti.app.models.models_pti_task import DbClients
 from pti.app.validators.xml_file import XmlFilePTIValidator
 
 from tests.factories.database.organisation import OrganisationDatasetRevisionFactory
@@ -21,11 +22,15 @@ def test_get_violations_validates_file(m_pti_validator):
     xml_file = MagicMock(spec=StreamingBody)
     xml_file.read.return_value = b"dummycontent"
 
-    validator = XmlFilePTIValidator(
-        schema=BytesIO(),
+    clients = DbClients(
+        sql_db=MagicMock(spec=SqlDB),
         dynamodb=MagicMock(spec=DynamoDBCache),
         stop_point_client=MagicMock(spec=NaptanStopPointDynamoDBClient),
-        db=MagicMock(spec=SqlDB),
+    )
+
+    validator = XmlFilePTIValidator(
+        schema=BytesIO(),
+        db_clients=clients,
         txc_data=TXCData.model_construct(),
     )
     validator._validator = m_pti_validator.return_value
