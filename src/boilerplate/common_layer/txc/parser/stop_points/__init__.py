289b5c450c917feb20a9de90a23cfff8cf6622ac
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-01-28 12:56:29 +0000

    add tests

--- src/boilerplate/common_layer/txc/parser/stop_points/__init__.py
+++ src/boilerplate/common_layer/txc/parser/stop_points/__init__.py
@@ -2,6 +2,30 @@
 Stop Point Parser Module Exports
 """
 
-from .stop_points import parse_stop_points, parse_txc_stop_point
+from .parse_stop_point_classification import (
+    parse_bearing_structure,
+    parse_bus_stop_structure,
+    parse_marked_point_structure,
+    parse_on_street_structure,
+)
+from .stop_points import (
+    parse_descriptor_structure,
+    parse_location_structure,
+    parse_place_structure,
+    parse_stop_classification_structure,
+    parse_stop_points,
+    parse_txc_stop_point,
+)
 
-__all__ = ["parse_stop_points", "parse_txc_stop_point"]
+__all__ = [
+    "parse_stop_points",
+    "parse_txc_stop_point",
+    "parse_bus_stop_structure",
+    "parse_on_street_structure",
+    "parse_descriptor_structure",
+    "parse_location_structure",
+    "parse_place_structure",
+    "parse_stop_classification_structure",
+    "parse_marked_point_structure",
+    "parse_bearing_structure",
+]
--- tests/timetables_etl/etl/txc/parser/txc_stoppoint/test_txc_classification_marked_point.py
+++ tests/timetables_etl/etl/txc/parser/txc_stoppoint/test_txc_classification_marked_point.py
@@ -4,9 +4,13 @@ Test MarkedPoint
 
 import pytest
 from common_layer.txc.models import BearingStructure, MarkedPointStructure
+from common_layer.txc.models.txc_stoppoint import UnmarkedPointStructure
 from common_layer.txc.parser.stop_points import (
     parse_bearing_structure,
     parse_marked_point_structure,
+)
+from common_layer.txc.parser.stop_points.parse_stop_point_classification import (
+    parse_unmarked_point_structure,
 )
 from lxml.etree import fromstring
 
@@ -81,3 +85,45 @@ def test_parse_marked_point_structure(
     """
     marked_point_xml = fromstring(marked_point_xml_str)
     assert parse_marked_point_structure(marked_point_xml) == expected_result
+
+
+@pytest.mark.parametrize(
+    "unmarked_point_xml_str, expected_result",
+    [
+        pytest.param(
+            """
+            <UnmarkedPoint>
+                <Bearing>
+                    <CompassPoint>N</CompassPoint>
+                </Bearing>
+            </UnmarkedPoint>
+            """,
+            UnmarkedPointStructure(Bearing=BearingStructure(CompassPoint="N")),
+            id="Valid UnmarkedPointStructure",
+        ),
+        pytest.param(
+            "<UnmarkedPoint></UnmarkedPoint>",
+            None,
+            id="Missing Bearing",
+        ),
+        pytest.param(
+            """
+            <UnmarkedPoint>
+                <Bearing>
+                    <CompassPoint>InvalidPoint</CompassPoint>
+                </Bearing>
+            </UnmarkedPoint>
+            """,
+            None,
+            id="Invalid Bearing",
+        ),
+    ],
+)
+def test_parse_unmarked_point_structure(
+    unmarked_point_xml_str: str, expected_result: UnmarkedPointStructure | None
+):
+    """
+    Test Parsing an Unmarked Point Structure
+    """
+    unmarked_point_xml = fromstring(unmarked_point_xml_str)
+    assert parse_unmarked_point_structure(unmarked_point_xml) == expected_result
