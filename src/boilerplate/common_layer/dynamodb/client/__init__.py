5b5800ef797c2c1d2cff44ee8e7e2b6bf4da9fa3
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-02-03 13:38:25 +0000

    refactor(DynamoDB): Add narrower types to DynamoDB Naptan Client
    - Copy AttributeValueTypeDef from boto3 stubs into models file
    - Add `get_by_atco_code` function to getch a single item

--- src/boilerplate/common_layer/dynamodb/client/__init__.py
+++ src/boilerplate/common_layer/dynamodb/client/__init__.py
@@ -4,7 +4,8 @@ Module for DynamoDB clients
 
 from .base import DynamoDB, DynamoDBSettings
 from .cache import DynamoDBCache, DynamoDbCacheSettings
-from .naptan_stop_points import NaptanDynamoDBSettings, NaptanStopPointDynamoDBClient
+from .naptan_stop_points import NaptanStopPointDynamoDBClient
+from .settings import NaptanDynamoDBSettings
 
 __all__ = [
     "DynamoDB",
