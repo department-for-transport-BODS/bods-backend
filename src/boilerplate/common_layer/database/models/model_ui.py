6bb4371b3b91f50327a1304ee2c0301f627d1b6d
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-02-04 16:40:07 +0000

    Add Info strings

--- src/boilerplate/common_layer/database/models/model_ui.py
+++ src/boilerplate/common_layer/database/models/model_ui.py
@@ -8,6 +8,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class UiLta(BaseSQLModel):
+    """
+    Stores the mapping of local authorities to a local authority name
+    Local Authorities from the registration data
+    This supports the ability to group local authorities into a combined authorities view.
+    It is displayed on the BODS UI.
+    This relationship is manually created using Django.
+    """
+
     __tablename__ = "ui_lta"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
