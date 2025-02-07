2fee8564e9c361ea6ce83908028218badc24010f
Andy Crow <crow.andy96@gmail.com>
2025-02-07 09:11:55 -0300

    Fix typing error in pt_iterator

--- src/periodic_tasks/pt_iterator.py
+++ src/periodic_tasks/pt_iterator.py
@@ -1,8 +1,9 @@
-import boto3
-import os
 import logging
+import os
 from os import environ
 from sys import stdout
+
+import boto3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(environ.get("LOG_LEVEL", "DEBUG"))
@@ -20,6 +21,9 @@ def lambda_handler(event, context):
     index = event["iterator"]["index"] + 1
 
     target_functions = os.environ.get("TARGET_FUNCTION_NAMES")
+    if not target_functions:
+        raise ValueError("No target functions found in env TARGET_FUNCTION_NAMES")
+
     logger.info(f"Target functions to invoke: {target_functions}")
 
     function_names = target_functions.split(",")
