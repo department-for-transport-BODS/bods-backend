181ed7ffeb3a2fc9696e27fd503f6edadc71a9e3
Rob Steel <142216849+robsteel-kpmg@users.noreply.github.com>
2025-02-19 19:54:25 +0000

    feat(FaresTemplate): Add in additional nested stack to accommodate fares etl (#296)
    Description:
    Added additional nest stack template to accommodate new Fares ETL
    process. This statemachine reuses a number of existing resources that
    were created for the Timetables ETL process, and have since been adapted
    for dual purpose.
    
    Key Details:
    - Included placeholder Lambda function that can be repurposed to be a
    standalone lambda for integration into the statemachine
    - Updated template to include nested stack for Fares ETL
    - Re-using existing functions from Timetables ETL template
    - Removed some resources no longer required
    
    JIRA: https://kpmgengineering.atlassian.net/browse/BODS-8204

---
+++ src/fares_etl/placeholder/app/placeholder.py
@@ -0,0 +1,20 @@
+"""
+PlaceholderLambda
+"""
+
+from typing import Any
+
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from structlog.stdlib import get_logger
+
+log = get_logger()
+
+
+def lambda_handler(event: dict[str, Any], _context: LambdaContext) -> dict[str, Any]:
+    """
+    Main lambda handler
+    """
+    input_data = event  # pylint: disable=unused-variable
+    log.info("Placeholder Lambda - No functionality")
+
+    return {"statusCode": 200, "body": "Successfully ran the placeholder lambda"}
