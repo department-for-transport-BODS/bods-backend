5f2ddcc67acdb78000a8845543dccc823e9a175f
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-03-07 13:57:19 +0000

    refactor(timetables-etl): Add extra tracks generation logging (#340)
    There seems to be a discrepancy between generating tracks between the
    local-etl and the test environment where processing a tracks file with
    no existing tracks results in no new tracks being added
    
    To aid debugging this PR adds extra logging for the steps including
    logging
    
    It also uses a set for creating the stop pairs in the TXC to reduce the
    size of the DB query
    
    - Add logging to creating TransmodelTracks not in the DB
    - Use a set for the list of stop pairs to query the DB for, to reduce
    the query size
    
    JIRA: https://kpmgengineering.atlassian.net/browse/BODS-8371

--- src/boilerplate/common_layer/xml/txc/helpers/routes.py
+++ src/boilerplate/common_layer/xml/txc/helpers/routes.py
@@ -8,11 +8,11 @@ from ..models.txc_route import TXCRouteSection
 def extract_stop_point_pairs(
     route_sections: list[TXCRouteSection],
 ) -> list[tuple[str, str]]:
-    """Extract From/To stop point reference pairs from route sections."""
-    stop_point_pairs: list[tuple[str, str]] = []
+    """Extract unique From/To stop point reference pairs from route sections."""
+    stop_point_pairs: set[tuple[str, str]] = set()
 
     for section in route_sections:
         for route_link in section.RouteLink:
-            stop_point_pairs.append((route_link.From, route_link.To))
+            stop_point_pairs.add((route_link.From, route_link.To))
 
-    return stop_point_pairs
+    return list(stop_point_pairs)
