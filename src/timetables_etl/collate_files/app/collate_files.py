dc86277b452eeea6799351be3411f410cf3b193b
Rob Steel <robert.steel@kpmg.co.uk>
2025-03-11 13:10:57 +0000

    Docstrings

--- src/timetables_etl/collate_files/app/collate_files.py
+++ src/timetables_etl/collate_files/app/collate_files.py
@@ -6,4 +6,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 
 
 def lambda_handler(event: dict[str, Any], _context: LambdaContext) -> dict[str, Any]:
+    """
+    Lambda Handler for CollateFiles
+    """
     return {"statusCode": 200, "body": json.dumps(event)}
