f6933d064b8f00659003824973fd5120817bf817
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-03-19 12:10:23 +0000

    style(lint): add types to fix pyright strict issues

--- src/timetables_etl/etl/app/helpers/dataclasses/__init__.py
+++ src/timetables_etl/etl/app/helpers/dataclasses/__init__.py
@@ -4,3 +4,5 @@ Module exports for dataclasses
 
 from .lookups import ReferenceDataLookups
 from .stop_points import NonExistentNaptanStop
+
+__all__ = ["ReferenceDataLookups", "NonExistentNaptanStop"]
