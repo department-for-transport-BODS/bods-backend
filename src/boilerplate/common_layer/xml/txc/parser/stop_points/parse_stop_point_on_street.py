9e139965764c4664dbe7ba2e3c622744274ea642
andyecrow <38900570+andyecrow@users.noreply.github.com>
2025-03-25 12:20:44 -0300

    fix(ETLProcess): Fix creation of flexible service linestring (#413)

--- src/boilerplate/common_layer/xml/txc/parser/stop_points/parse_stop_point_on_street.py
+++ src/boilerplate/common_layer/xml/txc/parser/stop_points/parse_stop_point_on_street.py
@@ -11,9 +11,12 @@ from ....utils import get_element_text
 from ...models import (
     BusStopStructure,
     BusStopTypeT,
+    FlexibleZoneStructure,
+    LocationStructure,
     OnStreetStructure,
     TaxiStopClassificationStructure,
 )
+from ..stop_points import parse_location_structure
 from ..txc_types import parse_timing_status
 from .parse_stop_point_marked import (
     parse_marked_point_structure,
@@ -68,12 +71,34 @@ def parse_bus_stop_structure(bus_xml: _Element) -> BusStopStructure | None:
         log.warning("Missing MarkedPoint for marked bus stop type")
         return None
 
+    flexible_zone_xml = bus_xml.find("FlexibleZone")
+    flexible_zone = (
+        parse_flexible_zone(flexible_zone_xml)
+        if flexible_zone_xml is not None
+        else None
+    )
+
     return BusStopStructure(
         BusStopType=cast(BusStopTypeT, bus_stop_type),
         TimingStatus=timing_status,
         MarkedPoint=marked_point,
         UnmarkedPoint=unmarked_point,
+        FlexibleZone=flexible_zone,
     )
+
+
+def parse_flexible_zone(xml: _Element) -> FlexibleZoneStructure:
+    """
+    Parse the FlexibleZone structure within a Bus element
+
+    StopPoints -> StopPoint -> StopClassification -> OnStreet -> Bus -> FlexibleZone
+    """
+    location_elements = xml.findall("Location")
+    locations: list[LocationStructure] = []
+    for loc_xml in location_elements:
+        location = parse_location_structure(loc_xml)
+        locations.append(location)
+    return FlexibleZoneStructure(Location=locations or None)
 
 
 def parse_taxi_structure(taxi_xml: _Element) -> TaxiStopClassificationStructure | None:
