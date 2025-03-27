e9abb3bfa8126f586242feb28acc1b8fb2223748
Andy Crow <andy.crow@doza.consulting>
2025-03-27 10:28:24 -0300

    Remove unused import

--- src/timetables_etl/etl/app/transform/models_context.py
+++ src/timetables_etl/etl/app/transform/models_context.py
@@ -10,7 +10,6 @@ from common_layer.database.models import (
     NaptanStopPoint,
     TransmodelServicePattern,
     TransmodelServicePatternStop,
-    TransmodelStopActivity,
     TransmodelVehicleJourney,
 )
 from common_layer.xml.txc.models import (
