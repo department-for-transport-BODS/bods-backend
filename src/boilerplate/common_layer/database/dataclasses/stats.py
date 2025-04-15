ca415b5cda89474b5977e9dfbbd110759608d7a1
andyecrow <38900570+andyecrow@users.noreply.github.com>
2025-04-15 14:58:12 +0200

    fix(GenerateOutputZip): Populate missing revision metadata fields (#464)
    In this PR we add some code to the Generate Output Zip step to:
    - Build a set of stats based on all services/txc file attributes
    processed during the ETL step
    - Update the revision fields with those stats
    (`publisher_creation_datetime`, `publisher_modified_datetime`,
    `first_expiring_service`, `last_expiring_service`,
    `first_service_start`)
    
    JIRA: https://kpmgengineering.atlassian.net/browse/BODS-8759

---
+++ src/boilerplate/common_layer/database/dataclasses/stats.py
@@ -0,0 +1,40 @@
+"""
+Dataclasses for reporting stats
+"""
+
+from dataclasses import dataclass
+from datetime import date, datetime
+
+
+@dataclass
+class RevisionStats:
+    """
+    Summary stats for a Dataset Revision
+    """
+
+    publisher_creation_datetime: datetime | None = None
+    publisher_modification_datetime: datetime | None = None
+    first_expiring_service: date | None = None
+    last_expiring_service: date | None = None
+    first_service_start: date | None = None
+
+
+@dataclass
+class ServiceStats:
+    """
+    Summary stats for a group of Services
+    """
+
+    first_service_start: date | None
+    first_expiring_service: date | None
+    last_expiring_service: date | None
+
+
+@dataclass
+class TXCFileStats:
+    """
+    Summary stats for a group of TXC File Attributes
+    """
+
+    first_creation_datetime: datetime | None
+    last_modification_datetime: datetime | None
