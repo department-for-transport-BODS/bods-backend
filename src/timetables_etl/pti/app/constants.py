7170e1afa24caa11e90c0f670fae615eb19909be
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-04-15 16:04:20 +0100

    Update Constants

--- src/timetables_etl/pti/app/constants.py
+++ src/timetables_etl/pti/app/constants.py
@@ -4,6 +4,5 @@ Constants
 
 from pathlib import Path
 
-PTI_SCHEMA_PATH = Path(__file__).parent / "pti_schema.json"
-REF_URL = "https://pti.org.uk/system/files/files/TransXChange_UK_PTI_Profile_v1.1.A.pdf"
-NAMESPACE = {"x": "http://www.transxchange.org.uk/"}
+PTI_SCHEMA_PATH: Path = Path(__file__).parent / "pti_schema.json"
+NAMESPACE: dict[str, str] = {"x": "http://www.transxchange.org.uk/"}
