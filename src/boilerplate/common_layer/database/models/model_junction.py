956f654dcbfe9f885c1c99d42f3eb36fb5162650
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2025-04-25 13:44:35 +0100

    feat(etl-timetables): populate organisation_datasetrevision_localities and organisation_datasetrevision_admin_areas (#492)
    These two tables are populated at the end of the celery etl by the
    add_feed_associations
    It was overlooked due to the similar transmodel tables.
    
    - Add DB Models and Repos
    - OrganisationDatasetRevisionAdminAreas +
    OrganisationDatasetRevisionAdminAreasRepo
    - OrganisationDatasetRevisionLocalities +
    OrganisationDatasetRevisionLocalitiesRepo
    - Update the service_pattern_associations functios to generate the data
    then add to db
    
    
    
    JIRA: https://kpmgengineering.atlassian.net/browse/BODS-8897

--- src/boilerplate/common_layer/database/models/model_junction.py
+++ src/boilerplate/common_layer/database/models/model_junction.py
@@ -3,7 +3,7 @@ Models for Many to Many Relationhip Tables
 AKA: Associative Entity, Junction Tables, Jump Tables
 """
 
-from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .common import BaseSQLModel
@@ -37,6 +37,34 @@ class TransmodelServicePatternLocality(BaseSQLModel):
     locality_id: Mapped[str] = mapped_column(String(8), nullable=False)
 
 
+class OrganisationDatasetRevisionLocalities(BaseSQLModel):
+    """
+    Association Table between:
+        - organisation_datasetrevision (OrganisationDatasetRevision)
+        - naptan_locality (NaptanLocality)
+    """
+
+    __tablename__ = "organisation_datasetrevision_localities"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
+    datasetrevision_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey(
+            "organisation_datasetrevision.id", deferrable=True, initially="DEFERRED"
+        ),
+        nullable=False,
+    )
+    locality_id: Mapped[str] = mapped_column(
+        String(8),
+        ForeignKey(
+            "naptan_locality.gazetteer_id", deferrable=True, initially="DEFERRED"
+        ),
+        nullable=False,
+    )
+
+    __table_args__ = (UniqueConstraint("datasetrevision_id", "locality_id"),)
+
+
 class TransmodelServicePatternAdminAreas(BaseSQLModel):
     """
     Association Table between:
@@ -49,6 +77,32 @@ class TransmodelServicePatternAdminAreas(BaseSQLModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
     servicepattern_id: Mapped[int] = mapped_column(Integer, nullable=False)
     adminarea_id: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+class OrganisationDatasetRevisionAdminAreas(BaseSQLModel):
+    """
+    Association Table between:
+        - organisation_datasetrevision (OrganisationDatasetRevision)
+        - naptan_adminarea (NaptanAdminArea)
+    """
+
+    __tablename__ = "organisation_datasetrevision_admin_areas"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
+    datasetrevision_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey(
+            "organisation_datasetrevision.id", deferrable=True, initially="DEFERRED"
+        ),
+        nullable=False,
+    )
+    adminarea_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("naptan_adminarea.id", deferrable=True, initially="DEFERRED"),
+        nullable=False,
+    )
+
+    __table_args__ = (UniqueConstraint("datasetrevision_id", "adminarea_id"),)
 
 
 class TransmodelTracksVehicleJourney(BaseSQLModel):
