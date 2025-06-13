97520fc738a9b86fb5ed0ed33deb4cdfb4488049
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2025-06-13 12:25:07 +0100

    story/BODS-8995:fixed pylint issues

--- src/timetables_etl/generate_output_zip/app/output_processing.py
+++ src/timetables_etl/generate_output_zip/app/output_processing.py
@@ -5,11 +5,12 @@ Functions to create and upload zip files.
 import zipfile
 from io import BytesIO
 from typing import List, Tuple
+from dataclasses import dataclass
 
 from common_layer.aws.step import MapExecutionSucceeded
 from common_layer.s3 import S3
 from structlog.stdlib import get_logger
-from dataclasses import dataclass
+
 
 log = get_logger()
 
@@ -126,12 +127,10 @@ def generate_zip_file(
     ]
     counts = Counts()
     zip_buffer = BytesIO()
-    filename = original_object_key.split("/")[-1]
-    file_path = f"/tmp/{filename}"
 
     try:
-        success = get_original_zip(s3_client, original_object_key, file_path)
-        if not success:
+        file_path = f"/tmp/{original_object_key.split('/')[-1]}"
+        if not get_original_zip(s3_client, original_object_key, file_path):
             log.error(f"Failed to download source zip: {original_object_key}")
             return (BytesIO(), 0, 0)
 
