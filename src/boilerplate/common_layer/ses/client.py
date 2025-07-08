f5a51218b67edc4667dd35142af88baa4ee305f3
Amit Singh <amit.singh@kpmg.co.uk>
2025-07-08 17:43:45 +0530

    Added send email method for the email notification

---
+++ src/boilerplate/common_layer/ses/client.py
@@ -0,0 +1,52 @@
+from os import environ
+from typing import TYPE_CHECKING
+
+import boto3
+from structlog.stdlib import get_logger
+
+if TYPE_CHECKING:
+    from mypy_boto3_ses import SESClient
+
+log = get_logger()
+
+
+def send_email(emails: list[str], step: str, subject: str, content: str) -> bool:
+    """Send Email for serverless lambdas for ETL pipeline, AWS SES service will
+    be utilised in order to send the emails
+
+    Args:
+        emails (list[str]): list of recepients email ids 
+        step (str): From which step did we triggered the email from
+        subject (str): Subject content of the email
+        content (str): Body content of the email
+
+    Returns:
+        bool: Returns true always, because if failed to send an email, it should 
+        log the error but shouldn't raise an exception
+    """
+    try:
+        ses_client: SESClient = boto3.client("ses", region_name=environ.get("AWS_REGION", "eu-west-2"))  # type: ignore
+        from_email = environ.get(
+            "DEFAULT_FROM_EMAIL", "Bus Open Data Service <noreply@bods.com>"
+        )
+
+        ses_client.send_email(
+            Source=from_email,
+            Destination={
+                "ToAddresses": emails,
+            },
+            Message={
+                "Subject": {"Data": subject, "Charset": "UTF-8"},
+                "Body": {
+                    "Text": {"Data": content, "Charset": "UTF-8"},
+                },
+            },
+        )
+    except Exception:
+        log.error(
+            "Error ocurred while sending an email for ETL failure for step: ",
+            step=step,
+            exc_info=True,
+        )
+
+    return True
