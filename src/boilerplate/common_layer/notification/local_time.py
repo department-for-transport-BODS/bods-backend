28dd1db91b471797e9c903820310133d75f300f3
Amit Singh <amit.singh@kpmg.co.uk>
2025-07-15 12:15:55 +0530

    updated local time converter typecastings

--- src/boilerplate/common_layer/notification/local_time.py
+++ src/boilerplate/common_layer/notification/local_time.py
@@ -1,11 +1,12 @@
 """Email helper class for methods requred for time conversion"""
 
+from datetime import datetime
 from zoneinfo import ZoneInfo
 
 
 def localize_datetime_and_convert_to_string(
-    datetime_object, datetime_string_format: str = "%d-%m-%Y %H:%M"
-):
+    datetime_object: datetime, datetime_string_format: str = "%d-%m-%Y %H:%M"
+) -> str:
     """Convert datetime object to a given format
 
     Args:
@@ -17,5 +18,5 @@ def localize_datetime_and_convert_to_string(
     """
     # convert datetime using uktimezone
     uk_timezone = ZoneInfo("Europe/London")
-    localized_datetime = datetime_object.replace(tzinfo=uk_timezone)
+    localized_datetime: datetime = datetime_object.replace(tzinfo=uk_timezone)
     return localized_datetime.strftime(datetime_string_format)
