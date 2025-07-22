fde5f82ec3fdddbcc117a7e7f0319b466922af06
Andy Crow <andy.crow@doza.consulting>
2025-07-22 11:33:56 +0200

    Update batcher to write to s3 + update state machine definition

--- src/periodic_tasks/consolidate_tracks_batcher/app/handler_consolidate_tracks_batcher.py
+++ src/periodic_tasks/consolidate_tracks_batcher/app/handler_consolidate_tracks_batcher.py
@@ -2,13 +2,16 @@
 Lambda handler for creating batches to be processed by the Consolidate Tracks lambda
 """
 
+import json
 from typing import Any
+from uuid import uuid4
 
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from common_layer.database import SqlDB
 from common_layer.database.repos import TransmodelTrackRepo
 from common_layer.json_logging import configure_logging
+from common_layer.s3 import S3
 from pydantic import BaseModel, ValidationError
 from structlog.stdlib import get_logger
 
@@ -20,12 +23,19 @@ class ConsolidateTracksBatcherInput(BaseModel):
     """Input schema for Consolidate Tracks Lambda."""
 
     batch_size: int = 500
+    s3_bucket: str
+
+
+def write_batches_to_s3(bucket_name: str, batches: list[list[tuple[str, str]]]) -> str:
+    json_data = json.dumps(batches).encode("utf-8")
+    s3_key = f"consolidate-tracks-batch/{uuid4()}.json"
+    s3_handler = S3(bucket_name=bucket_name)
+    s3_handler.put_object(s3_key, json_data)
+    return s3_key
 
 
 @tracer.capture_lambda_handler
-def lambda_handler(
-    event: dict[str, Any], context: LambdaContext
-) -> list[list[tuple[str, str]]]:
+def lambda_handler(event: dict[str, Any], context: LambdaContext) -> dict[str, str]:
     """
     Lambda handler for batching distinct stop point pairs
     to be processed by the Consolidate Tracks lambda.
@@ -50,9 +60,12 @@ def lambda_handler(
     ]
 
     log.info(
-        "Returning batched stop points",
+        "Uploading batched stoppoints to S3",
         number_of_batches=len(batches),
         batch_size=input_data.batch_size,
     )
+    s3_key = write_batches_to_s3(input_data.s3_bucket, batches)
 
-    return batches
+    return {
+        "s3Key": s3_key,
+    }
