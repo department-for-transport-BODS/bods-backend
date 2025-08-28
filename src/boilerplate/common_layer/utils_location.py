95eb163aefc9b0e53c660163c253c73e1acb7d2b
surabhika <75682555+surabhika@users.noreply.github.com>
2025-08-28 15:08:56 +0100

    Revert "fix(ETLProcess): Add parsing for easting and northing coordinates for Tracks (#547)"
    This reverts commit 5ed88fa1d2163d4ce170079ac3843b67b39d5d29.

--- src/boilerplate/common_layer/utils_location.py
+++ src/boilerplate/common_layer/utils_location.py
@@ -5,25 +5,23 @@ EPSG:4326 = WGS84 (Logitude / Latitude)
 Instantiate Transformer once
 """
 
-from pyproj import CRS, Transformer
+from pyproj import Transformer
 
-BNG = CRS("EPSG:27700")
-WGS84 = CRS("EPSG:4326")
 osgrid_to_lonlat_transformer = Transformer.from_crs(
-    BNG,
-    WGS84,
+    "EPSG:27700",
+    "EPSG:4326",
     always_xy=True,
 )
 lonlat_to_osgrid_transformer = Transformer.from_crs(
-    WGS84,
-    BNG,
+    "EPSG:4326",
+    "EPSG:27700",
     always_xy=True,
 )
 
 
 def osgrid_to_lonlat(easting: float, northing: float) -> tuple[float, float]:
     """
-    Convert OSGB36 (BNG) coordinates to WGS84 lat/lon
+    Convert OSGB36 coordinates to WGS84 lat/lon
 
     Returns a Tuple of Floats with Longitude,Latitude in that order
     """
